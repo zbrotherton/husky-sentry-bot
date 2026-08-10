5b3d454a83b680c5b830938a68373e58d3c54328
Zane Brotherton <zanebrotherton@gmail.com>
2026-08-10 00:48:10 +0000

    Upgrade project to jazzy Remove otr dependencies

--- .devcontainer/setup.bash
+++ .devcontainer/setup.bash
@@ -3,9 +3,9 @@ cd /home/ros2_ws
 apt-get update --fix-missing
 apt-get upgrade -y
 
-. /opt/ros/humble/setup.sh
+. /opt/ros/jazzy/setup.sh
 
-echo '. /opt/ros/humble/setup.sh' >> ~/.bashrc
+echo '. /opt/ros/jazzy/setup.sh' >> ~/.bashrc
 
 echo ' if [ -d '/home/ros2_ws/install' ]; then 
     . /home/ros2_ws/install/setup.bash
